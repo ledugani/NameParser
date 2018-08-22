e598a100ed69adab06cb3c47145dbadb26f71262
unknown <tdugan85@gmail.com>
2018-08-21 21:36:01 -0500

    initial commit

---
+++ NameParser/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace NameParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // first name
+            Console.WriteLine("Enter your first name");
+            var firstName = Console.ReadLine();
+
+            foreach(var letter in firstName)
+            {
+                Console.WriteLine(letter);
+            }
+
+            // last name
+            Console.WriteLine("Enter your last name");
+            var lastName = Console.ReadLine();
+            var lastNameWithSpaces = "";
+
+            foreach(var letter in lastName.ToUpper())
+            {
+                lastNameWithSpaces += letter + " ";
+            }
+
+            Console.WriteLine(lastNameWithSpaces.Trim());
+
+            // middle name
+            Console.WriteLine("Do you have a middle name? (y/n)");
+            var hasMiddleName = Console.ReadLine();
+
+            if (hasMiddleName == "y")
+            {
+                Console.WriteLine("What is your middle name?");
+                var middleName = Console.ReadLine();
+                var i = 0;
+
+                foreach(var letter in middleName)
+                {
+                    Console.WriteLine(letter.ToString().PadLeft(++i));
+                }
+            }
+            else
+            {
+                Console.WriteLine("Well, that sucks.");
+            }
+
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
