f1680dc52d26804bcdccc252e770dbfad536f53f
Tom Dugan <tdugan85@gmail.com>
2018-08-28 21:15:46 -0500

    wip

---
+++ NameParser/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NameParser.Names;
+
+namespace NameParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instances
+            var firstName = new FirstName();
+            var lastName = new LastName();
+            var middleName = new MiddleName(firstName);
+
+            // first name
+            firstName.GetName();
+            firstName.PrintName();
+
+            // last name
+            lastName.GetName();
+            lastName.PrintName();
+
+            // middle name
+            middleName.GetName();
+            middleName.PrintName();
+
+            // full name and goodbye
+            Console.WriteLine($"Goodbye {firstName} {middleName} {lastName}. Press enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
